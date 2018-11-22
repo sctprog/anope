930c6e7a4ddacde0ed9a8f4e7d5814cf89ad21e4
Scott MacDonald <scott@sctprog.ca>
2018-11-22 07:21:22 +0000

    m_sql_authentication: Support for fetch all in array

---
+++ include/modules/m_sql_authentication.h
@@ -0,0 +1,138 @@
+﻿/*
+ * (C) 2012-2018 Anope Team
+ * Contact us at team@anope.org
+ *
+ * Please read COPYING and README for further details.
+ */
+
+//#include "account.h"
+#include "modules.h"
+
+
+//#include "module.h"
+#include "modules/sql.h"
+
+#ifndef M_SQL_AUTHENTICATION_H
+#define M_SQL_AUTHENTICATION_H
+
+class SQLAuthenticationResult : public SQL::Interface
+{
+private:
+	Reference<User> user;
+	IdentifyRequest *req;
+
+ public:
+	/**
+	 * Constructor
+	 * @param usr User record
+	 * @param request Identity request object
+	 */
+	SQLAuthenticationResult(User *usr, IdentifyRequest *request);
+
+	/**
+	 * Destructor
+	 */
+	~SQLAuthenticationResult();
+
+	/**
+	 * Executed if the query was successful
+	 * @param result Result object containing information about the query sent
+	 */
+	void OnResult(const SQL::Result &result) override;
+
+	/**
+	 * Executed if the database gave an error on the query
+	 * @param result Result object containing information about the query sent
+	 */
+	void OnError(const SQL::Result &result) override;
+};
+
+/**
+ * Module handler: Configuration handling, core hooks
+ */
+class ModuleSQLAuthentication : public Module
+{
+	/**
+	 * Name of SQL engine to use for authentication
+	 */
+	Anope::string engine;
+
+	/**
+	 * Query to use for authentication
+	 */
+	Anope::string query;
+
+	/**
+	 * Reason to send to user why they can not register a nickname
+	 * nickserv/register will not be disabled if this is empty
+	 */
+	Anope::string disable_reason;
+
+	/**
+	 * Reason to send to the user why they can not set an email address
+	 * nickserv/set/email will not be disabled if this is empty
+	 */
+	Anope::string disable_email_reason;
+
+public:
+	/**
+	 * Are the DB side nicknames in an array?
+	 */
+	bool nicksInArray;
+
+	/**
+	 * Fetch all nicknames on identify?
+	 */
+	bool populateAll;
+
+	/**
+	 * Push IRC side group changes to the DB?
+	 */
+	bool pushChanges;
+
+	/**
+	 * Query to use on adding an alias
+	 */
+	Anope::string pushQuery_add;
+
+	/**
+	 * Query to use on removing an alias
+	 */
+	Anope::string pushQuery_remove;
+
+	/**
+	 * The database service object to use for queries
+	 */
+	ServiceReference<SQL::Provider> SQL;
+
+	/**
+	 * Constructor
+	 * @param modname
+	 * @param creator
+	 */
+	ModuleSQLAuthentication(const Anope::string &modname, const Anope::string &creator);
+
+	/**
+	 * Called during startup and configuration reload
+	 * @param conf Configuration object
+	 */
+	void OnReload(Configuration::Conf *conf) override;
+
+	/**
+	 * Called before a command is executed
+	 * @param source User who sent the command
+	 * @param command Command used
+	 * @param params Any parameters included
+	 * @return EventReturn informing the handler if we're ok with the command going ahead with execution
+	 */
+	EventReturn OnPreCommand(CommandSource &source, Command *command, std::vector<Anope::string> &params) override;
+
+	/**
+	 * Callback, SQL service has returned a result
+	 * @param usr Requesting user's record
+	 * @param req Request object. See include/account.h
+	 */
+	void OnCheckAuthentication(User *usr, IdentifyRequest *req) override;
+};
+
+#endif // M_SQL_AUTHENTICATION_H
--- modules/extra/m_sql_authentication.cpp
+++ modules/extra/m_sql_authentication.cpp
@@ -1,148 +1,275 @@
-/*
- *
+﻿/*
  * (C) 2012-2018 Anope Team
  * Contact us at team@anope.org
  *
  * Please read COPYING and README for further details.
  */
 
-#include "module.h"
-#include "modules/sql.h"
+#include <string>
+#include <sstream>
 
-static Module *me;
+#include "account.h"
+#include "config.h"
+#include "modules/m_sql_authentication.h"
 
-class SQLAuthenticationResult : public SQL::Interface
+// So the authentication result object can find the module handler
+static ModuleSQLAuthentication *me;
+
+// Constructor
+SQLAuthenticationResult::SQLAuthenticationResult(User *usr, IdentifyRequest *request)
+	: SQL::Interface(me)
 {
-	Reference<User> user;
-	IdentifyRequest *req;
+	user = usr;
+	req = request;
+	req->Hold(me);
+}
 
- public:
-	SQLAuthenticationResult(User *u, IdentifyRequest *r) : SQL::Interface(me), user(u), req(r)
-	{
-		req->Hold(me);
-	}
-
-	~SQLAuthenticationResult()
-	{
-		req->Release(me);
-	}
-
-	void OnResult(const SQL::Result &r) anope_override
-	{
-		if (r.Rows() == 0)
-		{
-			Log(LOG_DEBUG) << "m_sql_authentication: Unsuccessful authentication for " << req->GetAccount();
-			delete this;
-			return;
-		}
-
-		Log(LOG_DEBUG) << "m_sql_authentication: Successful authentication for " << req->GetAccount();
-
-		Anope::string email;
-		try
-		{
-			email = r.Get(0, "email");
-		}
-		catch (const SQL::Exception &) { }
-
-		NickAlias *na = NickAlias::Find(req->GetAccount());
-		BotInfo *NickServ = Config->GetClient("NickServ");
-		if (na == NULL)
-		{
-			na = new NickAlias(req->GetAccount(), new NickCore(req->GetAccount()));
-			FOREACH_MOD(OnNickRegister, (user, na, ""));
-			if (user && NickServ)
-				user->SendMessage(NickServ, _("Your account \002%s\002 has been successfully created."), na->nick.c_str());
-		}
-
-		if (!email.empty() && email != na->nc->email)
-		{
-			na->nc->email = email;
-			if (user && NickServ)
-				user->SendMessage(NickServ, _("Your email has been updated to \002%s\002."), email.c_str());
-		}
-
-		req->Success(me);
-		delete this;
-	}
-
-	void OnError(const SQL::Result &r) anope_override
-	{
-		Log(this->owner) << "m_sql_authentication: Error executing query " << r.GetQuery().query << ": " << r.GetError();
-		delete this;
-	}
-};
-
-class ModuleSQLAuthentication : public Module
+// Destructor
+SQLAuthenticationResult::~SQLAuthenticationResult()
 {
-	Anope::string engine;
-	Anope::string query;
-	Anope::string disable_reason, disable_email_reason;
+	req->Release(me);
+}
 
-	ServiceReference<SQL::Provider> SQL;
+/*
+ * Callback from the database engine
+ * This is executed after our query has been sent and returned and there are no errors with it.
+ * There may or may not be rows in the set.
+ */
+void SQLAuthenticationResult::SQLAuthenticationResult::OnResult(const SQL::Result &result)
+{
+	BotInfo *NickServ = Config->GetClient("NickServ");
 
- public:
-	ModuleSQLAuthentication(const Anope::string &modname, const Anope::string &creator) : Module(modname, creator, EXTRA | VENDOR)
+	Log(LOG_DEBUG) << "m_sql_authentication: received an answer from the remote database";
+
+	// On 0 results there simply is no match to the username/password combo
+	if (result.Rows() == 0)
 	{
-		me = this;
-
+		Log(LOG_DEBUG) << "m_sql_authentication: Unsuccessful authentication for " << req->GetAccount();
+		delete this;
+		return;
 	}
 
-	void OnReload(Configuration::Conf *conf) anope_override
+	// On more than one match we have a serious security problem.
+	// Could be a bad query or a misconfigured/compromised remote database.
+	if (result.Rows() != 1)
 	{
-		Configuration::Block *config = conf->GetModule(this);
-		this->engine = config->Get<const Anope::string>("engine");
-		this->query =  config->Get<const Anope::string>("query");
-		this->disable_reason = config->Get<const Anope::string>("disable_reason");
-		this->disable_email_reason = config->Get<Anope::string>("disable_email_reason");
+		Log(LOG_DEBUG)
+			<< "m_sql_authentication: Security problem! Expected 1 or 0 row matches for account authentication, got " << result.Rows() << "!\n"
+			<< " - User: " << req->GetAccount() << '\n'
+			<< " - Query was" << result.finished_query;
 
-		this->SQL = ServiceReference<SQL::Provider>("SQL::Provider", this->engine);
+		if (user && NickServ)
+			user->SendMessage(NickServ, _("Unable to authenticate: duplicate remote matches returned. Please notify services administrator."));
+
+		delete this;
+		return;
 	}
 
-	EventReturn OnPreCommand(CommandSource &source, Command *command, std::vector<Anope::string> &params) anope_override
+	Log(LOG_DEBUG) << "m_sql_authentication: Successful authentication for " << req->GetAccount();
+
+	// Grab what we need from the SQL result set
+	Anope::string email;
+	Anope::string unparsed_nicks;
+	try
 	{
-		if (!this->disable_reason.empty() && (command->name == "nickserv/register" || command->name == "nickserv/group"))
-		{
-			source.Reply(this->disable_reason);
-			return EVENT_STOP;
-		}
+		email = result.Get(0, "email");
 
-		if (!this->disable_email_reason.empty() && command->name == "nickserv/set/email")
-		{
-			source.Reply(this->disable_email_reason);
-			return EVENT_STOP;
-		}
+		if (me->nicksInArray && me->populateAll)
+			unparsed_nicks = result.Get(0, "nicknames");
+	}
+	catch (const SQL::Exception &ex)
+	{
+		if (email.empty())
+			Log(LOG_DEBUG) << "m_sql_authentication: Expected email information in query result: " << ex.GetReason();
 
-		return EVENT_CONTINUE;
+		else if (unparsed_nicks.empty() && me->nicksInArray && me->populateAll)
+			Log(LOG_DEBUG) << "m_sql_authentication: Expected nickname array in result set: " << ex.GetReason();
 	}
 
-	void OnCheckAuthentication(User *u, IdentifyRequest *req) anope_override
+	// The alias record for the nickname the user is currently using
+	NickAlias *authAlias = nullptr;
+
+	// The core record the alias belongs to.
+	// We use this to prevent multiple unique accounts for the same person
+	NickCore *authCore = nullptr;
+
+	// Which aliases are we going to add to the database?
+	std::vector<Anope::string> needAdding;
+
+	// The nick array returns to us from the server looking like:
+	// {nick1,nick2,nick3}
+	if (me->nicksInArray && me->populateAll)
 	{
-		if (!this->SQL)
+		// Remove the 2 braces on the outside and any whitespace since we're at it
+		unparsed_nicks = unparsed_nicks.trim("\r\t\n {}");
+
+		std::vector<Anope::string> nicknames;
+		Anope::string token;
+
+		// Separate the nicknames into a vector.
+		// Anope is helpful enough to provide a specialised tokenizer for CSV
+		commasepstream css(unparsed_nicks);
+		while (css.GetToken(token))
+			nicknames.emplace_back(token);
+
+		// Which aliases are in the remote database but not present in services?
+		for (auto t_nick :nicknames)
 		{
-			Log(this) << "Unable to find SQL engine";
-			return;
+			NickAlias *t_alias = NickAlias::Find(t_nick);
+
+			// Found the core account
+			if (t_alias != nullptr)
+				authCore = t_alias->nc;
+
+			if (req->GetAccount().lower() == t_nick.lower())
+				authAlias = t_alias;
+
+			// We're only adding aliases to this list that are different from the one the user is trying to auth with
+			else if (t_alias == nullptr)
+				needAdding.emplace_back(t_nick);
 		}
-
-		SQL::Query q(this->query);
-		q.SetValue("a", req->GetAccount());
-		q.SetValue("p", req->GetPassword());
-		if (u)
-		{
-			q.SetValue("n", u->nick);
-			q.SetValue("i", u->ip.addr());
-		}
-		else
-		{
-			q.SetValue("n", "");
-			q.SetValue("i", "");
-		}
-
-
-		this->SQL->Run(new SQLAuthenticationResult(u, req), q);
-
-		Log(LOG_DEBUG) << "m_sql_authentication: Checking authentication for " << req->GetAccount();
 	}
-};
+
+	// If we're not looking for an array of nicknames just try to find the services account
+	else
+		authAlias = NickAlias::Find(req->GetAccount());
+
+	if (authAlias == nullptr)
+	{
+		// Only create a new core account if none exists
+		if (authCore == nullptr)
+			authCore = new NickCore(req->GetAccount());
+
+		authAlias = new NickAlias(req->GetAccount(), authCore);
+
+		// Announce nick register to all other modules
+		FOREACH_MOD(OnNickRegister, (user, authAlias, ""));
+		if (user && NickServ)
+			user->SendMessage(NickServ, _("Your account \002%s\002 has been successfully created."), authAlias->nick.c_str());
+	}
+
+	// Update email record if it doesn't match our internal database
+	if (!email.empty() && email != authAlias->nc->email)
+	{
+		authAlias->nc->email = email;
+		if (user && NickServ)
+			user->SendMessage(NickServ, _("Your email has been updated to \002%s\002."), email.c_str());
+	}
+
+	/*
+	 * If populate all is true and there are missing aliases in the services set they will now be in our vector
+	 * Iterate the vector and create those aliases now.
+	 */
+	Anope::string messageAdded = "The following alias(es) have been added to your account:";
+	bool onFirst = true;
+	for (auto &nick :needAdding)
+	{
+		if (authCore != nullptr)
+		{
+			auto *t_alias = new NickAlias(nick, authCore);
+			FOREACH_MOD(OnNickRegister, (user, t_alias, "")); // Announce creation to other modules
+
+			// Add a comma to separate nicknames as necessary
+			if (!onFirst)
+				messageAdded += ',';
+
+			messageAdded += " \002" + nick + '\002';
+			onFirst = false;
+		}
+	}
+
+	// This will be false if no aliases were created.
+	if (!onFirst)
+		user->SendMessage(NickServ, messageAdded);
+
+	req->Success(me);
+	delete this;
+}
+
+/*
+ * Callback from the database engine
+ * This is executed after our query has been sent and returned and there are errors.
+ */
+void SQLAuthenticationResult::OnError(const SQL::Result &result)
+{
+	Log(this->owner) << "m_sql_authentication: Error executing query " << result.GetQuery().query << ": " << result.GetError();
+	delete this;
+}
+
+// Constructor
+ModuleSQLAuthentication::ModuleSQLAuthentication(const Anope::string &modname, const Anope::string &creator)
+ : Module(modname, creator, EXTRA | VENDOR)
+{
+	me = this;
+}
+
+// Executed during startup or operserv/reload
+void ModuleSQLAuthentication::OnReload(Configuration::Conf *conf)
+{
+	Configuration::Block *config = conf->GetModule(this);
+	this->engine = config->Get<const Anope::string>("engine");
+	this->query =  config->Get<const Anope::string>("query");
+	this->disable_reason = config->Get<const Anope::string>("disable_reason");
+	this->disable_email_reason = config->Get<Anope::string>("disable_email_reason");
+	this->nicksInArray = config->Get<bool>("nicknames_array");
+	this->populateAll = config->Get<bool>("populate_all");
+	this->pushChanges = config->Get<bool>("push_changes");
+	this->pushQuery_add = config->Get<Anope::string>("push_query_add");
+	this->pushQuery_remove = config->Get<Anope::string>("push_query_remove");
+	this->SQL = ServiceReference<SQL::Provider>("SQL::Provider", this->engine);
+}
+
+// Executed after any command has been received by services but not yet processed.
+EventReturn ModuleSQLAuthentication::OnPreCommand(CommandSource &source, Command *command, std::vector<Anope::string> &params)
+{
+	if (!this->disable_reason.empty() && (command->name == "nickserv/register" || command->name == "nickserv/group"))
+	{
+		source.Reply(this->disable_reason);
+		return EVENT_STOP;
+	}
+
+	if (!this->disable_email_reason.empty() && command->name == "nickserv/set/email")
+	{
+		source.Reply(this->disable_email_reason);
+		return EVENT_STOP;
+	}
+
+	return EVENT_CONTINUE;
+}
+
+/*
+ * Executed if there is an authenticate request for any reason
+ *
+ * Most commonly it will be a nickserv/identify event but it could also be from the
+ * web service, if active.
+ */
+void ModuleSQLAuthentication::OnCheckAuthentication(User *usr, IdentifyRequest *req)
+{
+	if (!this->SQL)
+	{
+		Log(this) << "Unable to find SQL engine";
+		return;
+	}
+
+	SQL::Query queryData(this->query);
+	queryData.SetValue("a", req->GetAccount());
+	queryData.SetValue("p", req->GetPassword());
+	if (usr)
+	{
+		queryData.SetValue("n", usr->nick);
+		queryData.SetValue("i", usr->ip.addr());
+	}
+
+	// Logging in via web interface, it's possible there is no current user record
+	else
+	{
+		queryData.SetValue("n", "");
+		queryData.SetValue("i", "");
+	}
+
+	this->SQL->Run(new SQLAuthenticationResult(usr, req), queryData);
+
+	Log(LOG_DEBUG) << "m_sql_authentication: Checking authentication for " << req->GetAccount();
+}
 
 MODULE_INIT(ModuleSQLAuthentication)
